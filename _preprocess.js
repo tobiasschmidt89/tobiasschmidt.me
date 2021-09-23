3aca064f09953a34b3e2de160c00c12e5730d6a6
Tobias Schmidt <contact.tobias.schmidt@gmail.com>
2021-09-23 14:51:07 +0200

    Change _data hack data due to lume update

--- _preprocess.js
+++ _preprocess.js
@@ -1,7 +1,7 @@
 // Workarround to make data available for njk groupby filter - will be resolved by Lume in upcoming version
 const addFrontmatterYear = [
     ['.html'],
-    (page) => (page._data.year = page.data.date.getFullYear())
+    (page) => (page.data.year = page.data.date.getFullYear())
 ]
 
 
