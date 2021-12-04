46977606abb69b00626b20001d33a26780faa6ee
Tobias Schmidt <contact.tobias.schmidt@gmail.com>
2021-12-04 10:58:42 +0100

    Fix slash in self closing meta tags.

--- src/_components/Meta.tmpl.js
+++ src/_components/Meta.tmpl.js
@@ -1,13 +1,10 @@
 export default ({title, description, site, author, url, tags}) => `
 <!-- Meta -->
 <title>${title} | ${author?.name}</title>
-<meta name="title" content="${title} | ${author?.name}">
+<meta name="title" content="${title} | ${author?.name}" />
 <meta name="description" content="${description}" />
 <meta name="keywords" content="${tags}" />
 <meta name="copyright" content="${author?.name}<${author?.email}>" />
 <meta name="author" content="${author?.name}<${author?.email}>" />
-<meta name="robots" content="index,follow" />
-<meta name="referrer" content="no-referrer">
-<link rel="canonical" href="${new URL(url, site?.url)}">
 <link type="text/plain" rel="author" href="${new URL('humans.txt', site?.url)}" />
 `
