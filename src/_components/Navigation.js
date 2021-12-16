c8400e38da98d9e1db2deabf2c88319b370e80e7
Tobias Schmidt <contact.tobias.schmidt@gmail.com>
2021-12-16 11:30:27 +0100

    Implement logical css properties to support all text writing modes

--- src/_components/Navigation.js
+++ src/_components/Navigation.js
@@ -2,17 +2,17 @@ export default ({prevPost = "", nextPost = ""}) => `
 <footer>
     ${nextPost && `
     <div>
-        <span>Older</span>
+        Older<br>
         <a href="${nextPost.data.url}" rel="next">${nextPost.data.title}</a>
     </div>
     `}
     ${prevPost && `
         <div>
-            <span>Newer</span>
+            Newer<br>
             <a href="${prevPost.data.url}" rel="prev">${prevPost.data.title}</a>
         </div>
     `}
-    <div class=".full">
+    <div>
         <a href="/" rel="home">Back to Index</a>
     </div>
 </footer>
